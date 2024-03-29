2cd36b4dfa830107b465146329fe0a0157104cd8
vishad-tyagi <78733360+vishad-tyagi@users.noreply.github.com>
2024-03-29 15:50:06 +0530

    Android part change

--- .github/workflows/update_app_identifiers.sh
+++ .github/workflows/update_app_identifiers.sh
@@ -1,13 +1,34 @@
+#!/bin/bash
+
 
 PR_NUMBER=$1
 
-ANDROID_MANIFEST="android/app/src/main/AndroidManifest.xml"
-sed -i "s/android:label=\"Pocket Paint\"/android:label=\"Pocket Paint PR$PR_NUMBER\"/" $ANDROID_MANIFEST
-sed -i "s/package=\"org.catrobat.paintroid\"/package=\"org.catrobat.paintroid.pr$PR_NUMBER\"/" $ANDROID_MANIFEST
-#
-#INFO_PLIST="ios/Runner/Info.plist"
-#xmlstarlet ed -L -u "/plist/dict/key[text()='CFBundleName']/following-sibling::string[1]" -v "paintroid PR$PR_NUMBER" $INFO_PLIST
-#xmlstarlet ed -L -u "/plist/dict/key[text()='CFBundleIdentifier']/following-sibling::string[1]" -v "org.catrobat.paintroidflutter.pr$PR_NUMBER" $INFO_PLIST
+MAIN_MANIFEST="android/app/src/main/AndroidManifest.xml"
+DEBUG_MANIFEST="android/app/src/debug/AndroidManifest.xml"
+PROFILE_MANIFEST="android/app/src/profile/AndroidManifest.xml"
+
+if [ -f "$MAIN_MANIFEST" ]; then
+    echo "Updating $MAIN_MANIFEST with PR number $PR_NUMBER"
+    sed -i "s/package=\"org.catrobat.paintroid\"/package=\"org.catrobat.paintroid.pr$PR_NUMBER\"/" $MAIN_MANIFEST
+    sed -i "s/android:label=\"Pocket Paint\"/android:label=\"Pocket Paint PR$PR_NUMBER\"/" $MAIN_MANIFEST
+else
+    echo "$MAIN_MANIFEST does not exist."
+fi
+
+if [ -f "$DEBUG_MANIFEST" ]; then
+    echo "Updating $DEBUG_MANIFEST with PR number $PR_NUMBER"
+    sed -i "s/package=\"org.catrobat.paintroid\"/package=\"org.catrobat.paintroid.pr$PR_NUMBER\"/" $DEBUG_MANIFEST
+else
+    echo "$DEBUG_MANIFEST does not exist."
+fi
+
+if [ -f "$PROFILE_MANIFEST" ]; then
+    echo "Updating $PROFILE_MANIFEST with PR number $PR_NUMBER"
+    sed -i "s/package=\"org.catrobat.paintroid\"/package=\"org.catrobat.paintroid.pr$PR_NUMBER\"/" $PROFILE_MANIFEST
+else
+    echo "$PROFILE_MANIFEST does not exist."
+fi
+
 
 
 INFO_PLIST="ios/Runner/Info.plist"
